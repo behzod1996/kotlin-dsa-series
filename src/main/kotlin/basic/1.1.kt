6e9af3dd7e4194ba951aad639cef817bd90e7c38
Behzod Halil <behzoddev@gmail.com>
2022-10-31 17:37:56 +0900

    [basic::] Add 1.1

---
+++ src/main/kotlin/basic/1.1.kt
@@ -0,0 +1,32 @@
+package basic
+
+/**
+ * Constant time:
+ * A constant time algorithm is one that has
+ * the same running time regardless of the size of the input.
+ * The Big O notation for constant time is O(1).
+ */
+
+fun checkFirst(values: List<String>): String {
+    return if (values.firstOrNull() != null) {
+        values.first()
+    } else {
+        "No values"
+    }
+}
+
+fun checkFirstOrNull(values: List<String>): String? {
+    return if (values.firstOrNull() != null) {
+        values.first()
+    } else {
+        null
+    }
+}
+
+fun main() {
+    val names = listOf("Behzod")
+    val emptyNames = emptyList<String>()
+
+    println(checkFirst(names))
+    println(checkFirstOrNull(emptyNames))
+}
