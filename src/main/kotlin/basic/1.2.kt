7478c7a846cd232221bf9f9df7946de50e31e032
Behzod Halil <behzoddev@gmail.com>
2022-10-31 17:38:02 +0900

    [basic::] Add 1.2

---
+++ src/main/kotlin/basic/1.2.kt
@@ -0,0 +1,16 @@
+package basic
+
+/**
+ * Linear Time:
+ * Its complexity is usually the easiest to understand.
+ * As the amount of data increases, the running time increases by the same amount.
+ * The Big O notation for linear time is O(n).
+ */
+
+fun main() {
+    val names = listOf("Behzod","Oybek","Abdurahmon","Sardor")
+
+    for (name in names) {
+        println("Name is $name")
+    }
+}
