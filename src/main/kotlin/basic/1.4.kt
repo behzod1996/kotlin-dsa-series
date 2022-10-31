439e48cda29eb55a5a8b73f6858cc6162f17c310
Behzod Halil <behzoddev@gmail.com>
2022-10-31 17:58:55 +0900

    [basic::] Add 1.4

---
+++ src/main/kotlin/basic/1.4.kt
@@ -0,0 +1,16 @@
+package basic
+
+fun findNumber(value: Int, numbers: List<Int>): Boolean {
+    for (number in numbers) {
+        if (number == value) {
+            return true
+        }
+    }
+    return false
+}
+
+fun main() {
+    val numbers = listOf(1,2,3,4,5,6)
+    val result = findNumber(5,numbers)
+    print(result)
+}
